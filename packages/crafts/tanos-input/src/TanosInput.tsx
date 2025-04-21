cc9aface83a38db7de67c670a494024dddf71e9f
bgyu <bgyu@miridih.com>
2025-04-22 07:39:44 +0900

    Enhance TanosInput component: add accessibility feature for SVG titles, implement focus styles in CSS, and introduce TanosContext for animation control. Update stories to include TanosProvider and TanosTrigger for improved functionality.

--- packages/crafts/tanos-input/src/TanosInput.tsx
+++ packages/crafts/tanos-input/src/TanosInput.tsx
@@ -1,38 +1,74 @@
-import React, { useId } from 'react';
+import React, { useEffect, useId, useImperativeHandle, useRef } from 'react';
 import styles from './tanos-input.module.css';
 
-export const TanosInput = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>((props, ref) => {
+type TanosInputProps = React.ComponentProps<'input'> & {
+  setValue?: (value: string) => void;
+};
+
+export const TanosContext = React.createContext<{
+  startAnimationRef: React.MutableRefObject<() => void>;
+}>({
+  startAnimationRef: { current: () => {} },
+});
+
+export const useTanos = () => {
+  return React.useContext(TanosContext);
+};
+
+export const TanosProvider = ({ children }: { children: React.ReactNode }) => {
+  const startAnimationRef = useRef<() => void>(() => {});
+
+  return <TanosContext.Provider value={{ startAnimationRef }}>{children}</TanosContext.Provider>;
+};
+
+export const TanosTrigger = ({ children }: { children: React.ReactNode }) => {
+  const { startAnimationRef } = useTanos();
+
+  return <div onClick={() => startAnimationRef.current?.()}>{children}</div>;
+};
+
+export const TanosInput = React.forwardRef<HTMLInputElement, TanosInputProps>((props, ref) => {
+  const labelRef = useRef<HTMLLabelElement>(null);
   const id = useId();
+  const { setValue, ...inputProps } = props;
+  const { startAnimationRef } = useTanos();
+
+  useImperativeHandle(startAnimationRef, () => startAni);
+
+  const startAni = () => {
+    if (labelRef.current) {
+      labelRef.current.classList.add(styles.animate);
+    }
+  };
+
+  const resetAni = () => {
+    document.documentElement.style.setProperty('--tanos-progress', '0%');
+    if (labelRef.current) {
+      labelRef.current.classList.remove(styles.animate);
+    }
+  };
+
   return (
-    <label htmlFor={id} className={`${styles.container} ${styles.animate}`}>
-      <input id={id} className={`${styles.input} ${styles['masked-text']}`} type='text' ref={ref} {...props} />
+    <label
+      ref={labelRef}
+      htmlFor={id}
+      className={`${styles.container} ${styles.animate}`}
+      onAnimationEnd={() => {
+        props.setValue?.('');
+        setTimeout(() => {
+          resetAni();
+        }, 50);
+      }}
+    >
+      <input id={id} className={`${styles.input} ${styles['masked-text']}`} ref={ref} {...inputProps} />
       <input
+        {...inputProps}
         className={`${styles.input} ${styles.overlay}`}
         style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none' }}
         readOnly
-        type='text'
         ref={ref}
-        {...props}
       />
       <svg style={{ display: 'none' }}>
-        <filter id={'top-filter'}>
-          <feTurbulence
-            type='fractalNoise'
-            baseFrequency='1.8'
-            numOctaves='10'
-            result='turbulence'
-            seed={1}
-            stitchTiles={'noStitch'}
-          />
-          <feDisplacementMap
-            in2='turbulence'
-            in='SourceGraphic'
-            result='Source'
-            scale={20}
-            xChannelSelector='G'
-            yChannelSelector='R'
-          />
-        </filter>
         <filter id={'bottom-filter'}>
           <feTurbulence
             type='fractalNoise'
--- packages/crafts/tanos-input/stories/TanosInput.stories.tsx
+++ packages/crafts/tanos-input/stories/TanosInput.stories.tsx
@@ -1,13 +1,20 @@
 import type { Story } from '@ladle/react';
-import { TanosInput } from '../src/TanosInput';
+import { TanosInput, TanosProvider, TanosTrigger } from '../src/TanosInput';
 import { useState } from 'react';
 import './TanosInput.stories.css';
 
 export const Basic: Story = () => {
   const [value, setValue] = useState('우뿌ㅜ뿌뿌뿌ㅜ뻐ㅏㅣ우아ㅣ런이ㅓㄹ');
   return (
-    <div className='container'>
-      <TanosInput value={value} onChange={e => setValue(e.target.value)} />
-    </div>
+    <TanosProvider>
+      <div className='container'>
+        <TanosInput value={value} onChange={e => setValue(e.target.value)} setValue={setValue} />
+        <TanosTrigger>
+          <button type='button' className='btn'>
+            제출하기
+          </button>
+        </TanosTrigger>
+      </div>
+    </TanosProvider>
   );
 };
