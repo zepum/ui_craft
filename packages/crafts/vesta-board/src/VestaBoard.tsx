4e488ed5dfaf0a760f2747d9f0fc0e353f90e60d
bgyu <bgyu@miridih.com>
2025-04-28 07:57:45 +0900

    feat: integrate GSAP for animations in VestaBoard component and add CSS styles

--- packages/crafts/vesta-board/src/VestaBoard.tsx
+++ packages/crafts/vesta-board/src/VestaBoard.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
-import styles from './VestaBoard.module.css';
+import './VestaBoard.css';
 import { usePane } from '@core/debug';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const DEV_SETTINGS = {
   outline: true,
+  rotateX: 0,
 } as const;
 
 export const VestaBoard = () => {
@@ -15,18 +18,35 @@ export const VestaBoard = () => {
   useEffect(() => {
     if (!pane) return;
     pane.addBinding(DEV_SETTINGS, 'outline').on('change', setConfig);
+    pane.addBinding(DEV_SETTINGS, 'rotateX').on('change', setConfig);
   }, [pane]);
 
+  useGSAP(async () => {
+    const [forwardTop, forwardBottom, backwardTop, backwardBottom] = Array.from(
+      document.querySelectorAll('.object > div'),
+    );
+
+    const tl = gsap
+      .timeline()
+      .fromTo(backwardBottom, { rotateX: 180 }, { rotateX: 0 }, 0)
+      .fromTo(forwardTop, { rotateX: 0 }, { rotateX: -180 }, 0);
+
+    // tl.play();
+    // confirm the letters
+    // gsap.set([backwardTop, backwardBottom], { innerText: 'B' });
+    // gsap.set([forwardTop, forwardBottom], { innerText: 'C' });
+  });
+
   return (
-    <div data-outline={__DEV_config.outline} className={styles.object}>
+    <div data-outline={__DEV_config.outline} className={'object'}>
       {/* foward top */}
-      <div>A</div>
+      <div className={'forward forwardTop'}>A</div>
       {/* foward bottom */}
-      <div>A</div>
+      <div className={'forward forwardBottom'}>A</div>
       {/* backward top */}
-      {/* <div>A</div> */}
+      <div className={'backward backwardTop'}>B</div>
       {/* backward bottom */}
-      {/* <div></div> */}
+      <div className={'backward backwardBottom'}>B</div>
     </div>
   );
 };
