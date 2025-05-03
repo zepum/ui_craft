a5da65057899780fad6db679fa917d3d24e0afe6
gaki2 <qudrkr0815@gmail.com>
2025-05-03 13:25:21 +0900

    feat: VestaBoard 컴포넌트 구현

--- packages/crafts/vesta-board/stories/VestaBoard.stories.tsx
+++ packages/crafts/vesta-board/stories/VestaBoard.stories.tsx
@@ -1,5 +1,5 @@
 import type { Story } from '@ladle/react';
-import { VestaBoard } from '../src/VestaBoard';
+import { CHAR_SET, VestaBoard } from '../src/VestaBoard';
 import { useState } from 'react';
 
 const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
@@ -7,5 +7,41 @@ const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 export const DEFAULT: Story = () => {
   const [value, setValue] = useState('tanos_effect@avenue.com');
 
-  return <VestaBoard />;
+  return (
+    <VestaBoard
+      columnCount={12}
+      lines={[
+        {
+          text: 'hi world',
+          align: 'center',
+          color: 'oklch(0.64 0.296 240)',
+          charset: CHAR_SET,
+        },
+        {
+          text: '',
+          align: 'left',
+          color: '#fff',
+          charset: CHAR_SET,
+        },
+        {
+          text: '',
+          align: 'left',
+          color: '#fff',
+          charset: CHAR_SET,
+        },
+        {
+          text: '',
+          align: 'left',
+          color: '#fff',
+          charset: CHAR_SET,
+        },
+        {
+          text: '',
+          align: 'right',
+          color: '#fff',
+          charset: CHAR_SET,
+        },
+      ]}
+    />
+  );
 };
