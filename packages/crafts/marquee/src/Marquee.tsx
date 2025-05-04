351c25aff2d44ec8a1610324bbdcd0cb89f5355d
gaki2 <qudrkr0815@gmail.com>
2025-05-04 10:39:10 +0900

    feat: update Marquee component to use dynamic image list and enhance styling

--- packages/crafts/marquee/src/Marquee.tsx
+++ packages/crafts/marquee/src/Marquee.tsx
@@ -1,14 +1,27 @@
 import type { CSSProperties } from 'react';
 import styles from './Marquee.module.css';
 
+const prefix = 'https://tft-utils.s3.ap-northeast-2.amazonaws.com/ui-craft';
+const IMAGE_LIST = [
+  `${prefix}/de.jpg`,
+  `${prefix}/bolca.webp`,
+  `${prefix}/gun.webp`,
+  `${prefix}/rick.jpg`,
+  `${prefix}/elon.webp`,
+];
+
 export const Marquee = () => {
   return (
     <div style={{ '--count': 5 } as CSSProperties} className={styles.marqueeContainer}>
-      <div style={{ '--idx': 0 } as CSSProperties} className={styles.marqueeItem} />
-      <div style={{ '--idx': 1 } as CSSProperties} className={styles.marqueeItem} />
-      <div style={{ '--idx': 2 } as CSSProperties} className={styles.marqueeItem} />
-      <div style={{ '--idx': 3 } as CSSProperties} className={styles.marqueeItem} />
-      <div style={{ '--idx': 4 } as CSSProperties} className={styles.marqueeItem} />
+      {IMAGE_LIST.map((image, idx) => (
+        <img
+          key={image}
+          style={{ '--idx': idx } as CSSProperties}
+          className={styles.marqueeItem}
+          src={image}
+          alt={image}
+        />
+      ))}
     </div>
   );
 };
