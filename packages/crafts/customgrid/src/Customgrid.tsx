3afc862f08f42f9d1dba48cb21e1650b712697a4
kwonET <dlxl3652zz@gmail.com>
2025-07-11 18:08:40 +0900

    feat: add tweakpane control

--- packages/crafts/customgrid/src/Customgrid.tsx
+++ packages/crafts/customgrid/src/Customgrid.tsx
@@ -1,27 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Customgrid.module.css';
 
 // 1)  그리드 만들기
 // 2)  그리드 좌표 위  원 올 수 있게
 // 3) 그리드 조절하면 원 자동으로
-const Customgrid = () => {
-  const [gridData, setGridData] = useState({
-    rows: 5,
-    cols: 9,
-    grid: Array(5)
+
+export type CustomgridProps = {
+  rowVar: number;
+  colVar: number;
+  gapXVar: number;
+  gapYVar: number;
+  colorInVar: string;
+  colorOutVar: string;
+  size: number;
+};
+const Customgrid = ({ rowVar, colVar, gapXVar, gapYVar, colorInVar, colorOutVar, size }) => {
+  const [grid, setGrid] = useState(
+    Array(rowVar)
       .fill(0)
-      .map(() => Array(9).fill(0)),
-  });
+      .map(() => Array(colVar).fill(0)),
+  );
+  useEffect(() => {
+    setGrid(
+      Array(rowVar)
+        .fill(0)
+        .map(() => Array(colVar).fill(0)),
+    );
+  }, [rowVar, colVar]);
   return (
     <div className={styles.container}>
-      <div className={styles.grid} style={{ '--cols': gridData.cols, '--rows': gridData.rows } as React.CSSProperties}>
-        {gridData?.grid.map((row, rowIndex) =>
+      <div className={styles.grid} style={{ '--cols': colVar, '--rows': rowVar } as React.CSSProperties}>
+        {grid?.map((row, rowIndex) =>
           row.map((col, colIndex) => (
-            <div
-              key={`${rowIndex}-${colIndex}`}
-              className={styles.dot}
-              style={{ '--x': rowIndex, '--y': colIndex % gridData.cols } as React.CSSProperties}
-            ></div>
+            <div className={styles.dotContainer} key={`${rowIndex}-${colIndex}`}>
+              <div
+                className={styles.dot}
+                style={
+                  {
+                    '--x': rowIndex,
+                    '--y': colIndex % colVar,
+                    '--size': `${size}px`,
+                    '--color-in': colorInVar,
+                    '--color-out': colorOutVar,
+                  } as React.CSSProperties
+                }
+              ></div>
+            </div>
           )),
         )}
       </div>
--- packages/crafts/customgrid/stories/Customgrid.stories.tsx
+++ packages/crafts/customgrid/stories/Customgrid.stories.tsx
@@ -1,5 +1,93 @@
 import type { Story } from '@ladle/react';
+import { Binding, Pane } from '../../../core/debug/src';
 import Customgrid from 'src/Customgrid';
-export const Default: Story = () => {
-  return <Customgrid />;
+import { useState } from 'react';
+import { CustomgridProps } from 'src/Customgrid';
+
+const DEFAULT_CONFIG = {
+  rowVar: 3,
+  colVar: 5,
+  gapXVar: 50,
+  gapYVar: 50,
+  size: 50,
+  colorInVar: '#cefac4',
+  colorOutVar: 'rgb(30, 144, 255)',
 };
+export const Default: Story = () => {
+  const [devValue, setDevValue] = useState(DEFAULT_CONFIG);
+  return (
+    <div>
+      <Pane initialValue={DEFAULT_CONFIG} config={{ title: 'Custom Grid' }}>
+        <Binding
+          name='rowVar'
+          value={devValue.rowVar}
+          min={1}
+          max={20}
+          step={1}
+          onChange={value => {
+            setDevValue({ ...devValue, rowVar: value as number });
+          }}
+        />
+        <Binding
+          name='colVar'
+          value={devValue.colVar}
+          min={1}
+          max={20}
+          step={1}
+          onChange={value => {
+            setDevValue({ ...devValue, colVar: value as number });
+          }}
+        />
+        {/* <Binding
+          name='gapXVar'
+          value={devValue.gapXVar}
+          onChange={value => {
+            setDevValue({ ...devValue, gapXVar: value as number });
+          }}
+        />
+        <Binding
+          name='gapYVar'
+          value={devValue.gapYVar}
+          onChange={value => {
+            setDevValue({ ...devValue, gapYVar: value as number });
+          }}
+        /> */}
+        <Binding
+          name='colorInVar'
+          value={devValue.colorInVar}
+          onChange={value => {
+            setDevValue({ ...devValue, colorInVar: value as string });
+          }}
+        />
+        <Binding
+          name='colorOutVar'
+          value={devValue.colorOutVar}
+          onChange={value => {
+            setDevValue({ ...devValue, colorOutVar: value as string });
+          }}
+        />
+        <Binding
+          name='size'
+          value={devValue.size}
+          min={20}
+          max={200}
+          onChange={value => {
+            setDevValue({ ...devValue, size: value as number });
+          }}
+        />
+      </Pane>
+
+      <Customgrid
+        rowVar={devValue.rowVar}
+        colVar={devValue.colVar}
+        gapXVar={devValue.gapXVar}
+        gapYVar={devValue.gapYVar}
+        colorInVar={devValue.colorInVar}
+        colorOutVar={devValue.colorOutVar}
+        size={devValue.size}
+      />
+    </div>
+  );
+};
+
+// control things: row, cols, gap (x,y), colors, size (total 7 things)
